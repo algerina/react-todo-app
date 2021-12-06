d53a2f79e81c95b08fc304f4aa7c61b53ab821c0
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-06 22:46:37 +0100

    Add Style to the Todo list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 // component file
 import TodoContainer from "./components/TodoContainer"
+
+//stylesheet
+import "./App.css"
+
 ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById("root"))
 
 const element = <h1>Hello from Create React App</h1>
