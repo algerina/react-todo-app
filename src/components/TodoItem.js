a922081ffcc7c382d1ace4a16c2536ef8c3c2a83
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-07 00:47:53 +0100

    Add fuctionality to edit todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,24 +1,63 @@
-import React from "react"
-import styles from "./TodoItem.module.css"
+/* eslint-disable */
+import React, { useState } from 'react';
+import styles from './TodoItem.module.css';
 
-function TodoItem(props) {
+const TodoItem = (props) => {
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setEditing(false);
+    }
+  };
   const completedStyle = {
-    fontStyle: "italic",
-    color: "#595959",
+    fontStyle: 'italic',
+    color: '#595959',
     opacity: 0.4,
-    textDecoration: "line-through",
-  }
-  const { completed, id, title } = props.todo
-  return   <li className={styles.item}>
-  <input
-    type="checkbox"
-    className={styles.checkbox}
-    checked={completed}
-    onChange={() => props.handleChangeProps(id)}
-  />
-  <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
-  <span style={completed ? completedStyle : null}>{title}</span>
-</li>
-}
+    textDecoration: 'line-through',
+  };
 
-export default TodoItem
+  const { completed, id, title } = props.todo;
+
+  // handleEditing = () => {
+  //   console.log("edit mode activated")
+  // }
+
+  const viewMode = {};
+  const editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button type="button" onClick={() => props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input
+        type="text"
+        style={editMode}
+        className={styles.textInput}
+        value={title}
+        onChange={(e) => {
+          props.setUpdate(e.target.value, id);
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+
+export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
+/* eslint-disable */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 // component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from './components/TodoContainer';
 
-//stylesheet
-import "./App.css"
+// stylesheet
+import './App.css';
 
-ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById("root"))
+ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById('root'));
 
-const element = <h1>Hello from Create React App</h1>
+const element = <h1>Hello from Create React App</h1>;
 
 // ReactDOM.render(element, document.getElementById("root"))
