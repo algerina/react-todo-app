03e3936ff6edfca66e03172fccc7aa52bb1e07e0
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-07 15:35:02 +0100

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // /* eslint-disable */
 
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 // component file
-import TodoContainer from "./components/TodoContainer";
+import TodoContainer from './components/TodoContainer';
 
 // stylesheet
-import "./App.css";
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 const element = <h1>Hello from Create React App</h1>;
